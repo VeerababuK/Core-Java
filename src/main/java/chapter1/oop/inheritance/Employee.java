72b98aed24326f5a479a7e045a61186a60a75a00
Veerababu Kanumilli <veerababu.kanumilli@valuelabs.com>
2018-04-27 14:09:25 +0530

    minor changes

--- src/main/java/chapter1/oop/inheritance/Employee.java
+++ src/main/java/chapter1/oop/inheritance/Employee.java
@@ -2,4 +2,7 @@ package chapter1.oop.inheritance;
 
 public class Employee extends Person {
     private double salary;
+    // Emp ID, doj, designation, department, skill-set, employee_email
+
+    private Employee reportingManger;
 }
--- src/main/java/chapter1/oop/inheritance/Person.java
+++ src/main/java/chapter1/oop/inheritance/Person.java
@@ -2,4 +2,5 @@ package chapter1.oop.inheritance;
 
 public class Person {
     private String name;
+    // age, gender, dob, address, phone etc
 }
